0d61a8bcb491e7e4d8be636aa8e8405f74f9d7a3
Irfansyah001 <syahirfan545@gmail.com>
2024-03-19 14:39:37 +0700

    first commit

---
+++ DP_PerulangandanFungsi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+int i;
+string nama [5];
+
+for (i = ; i < 7; i++)
+{
+    cout << i << "." << "Haikal" << endl;
+}
+
+for(1=0;i < 5; i++){
+    cout <<"Masukan nama =";
+    cin >> nama[i];
+}
+
+for (i=0; i < 5;i++){
+    cout << endl ;
+    cout << "==============" << endl;
+    cout << "KUMPULAN NAMA-NAMA" << endl;
+
+for (i=0; i < 5;i++){
+
+    cout << "Namanya adalah" << nama[i] << endl;
